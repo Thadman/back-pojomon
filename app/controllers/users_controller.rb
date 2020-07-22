7b8c1deddf562519fedff23ac32e139a03833856
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-07-22 16:54:40 +1000

    when a user is created it also creates a monster

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
   def create
-    User.create(user_params)
+    user = User.create(user_params)
+    user.monsters.create(
+      name: "Egg", 
+      age: 0, 
+      weight: 1, 
+      hunger: 0, 
+      strength: 0, 
+      poop: 0, 
+      sick: false, 
+      death: 30, 
+      level: "Egg"
+    )
     render json: "user created", status: :created
   end
 
